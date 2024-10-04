c411eaf6277a7976bdd0825e705bec350f757a52
JustinG <justin.gestiere@gmail.com>
2024-10-04 11:02:59 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,80 @@
+function isEmpty (value){
+    return value != "" && value != null;
+};
+function isGreaterthanmontant (value){
+    return (value) >= 1000;
+};
+function isLowerthanmontant (value){
+    return (value) <= 1000000000;
+};
+function isGreaterthantaux (value){
+    return (value) >= 0.01;
+};
+function isLowerthantaux (value){
+    return (value) <= 100;
+};
+function isGreaterthanduree (value){
+    return (value) >= 1;
+};
+function isLowerthanduree (value){
+    return (value) <= 100;
+};
+function isValidInt(value) {
+    if (!isNaN(parseInt(value, 10))) {
+        return true;
+    } 
+    else {
+        return false;
+    }
+};
+function isValidFloat(value) {
+    return (value % 1 !== 0);
+};
+function verifmontant (value){
+    if(isEmpty(value) && isGreaterthanmontant(value) && isLowerthanmontant(value) && isValidInt(value) && !isValidFloat(value)){
+        return "";
+    }
+    else{
+        return " montant,";
+    }
+};
+function veriftaux (value){
+    if(isEmpty(value) && isGreaterthantaux(value) && isLowerthantaux(value) && isValidInt(value)){
+        return "";
+    }
+    else{
+        return " taux,";
+    }
+};
+function verifduree (value){
+    if(isEmpty(value) && isGreaterthanduree(value) && isLowerthanduree(value) && isValidInt(value) && !isValidFloat(value)){
+        return "";
+    }
+    else{
+        return " durée,";
+    }
+};
+
+document.getElementById("calcul").addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    let montantvide = document.querySelector("#montant").value;
+    let tauxvide = document.querySelector("#taux").value;
+    let dureevide = document.querySelector("#duree").value;
+
+    const msgmontant = verifmontant(montantvide);
+    const msgtaux = veriftaux(tauxvide);
+    const msgduree = verifduree(dureevide);
+
+    if(msgmontant == " montant," || msgtaux == " taux," || msgduree == " durée,") {        
+        document.getElementById("message").textContent = "Veuillez remplir les champs :"+ msgmontant + msgtaux + msgduree + " avec des données valides !";
+    }
+    else {
+        document.getElementById("message").textContent = ""; 
+    }
+});
+
+
+
+// Debut calcul tableau
